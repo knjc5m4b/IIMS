ca002857a47f6ce8a4252fe872da2931b4d437c1
yuchieh Kuan <yuchieh0725@gmail.com>
2019-05-19 22:43:01 +0800

    swiper 0519 test

--- IIMS/src/app/main/contents/Home/home.component.ts
+++ IIMS/src/app/main/contents/Home/home.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { SwiperComponent, SwiperDirective, SwiperConfigInterface} from 'ngx-swiper-wrapper';
 @Component({
     selector: 'app-home-component',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+    @ViewChild(SwiperComponent) componentRef: SwiperComponent;
+    @ViewChild(SwiperDirective) directiveRef: SwiperDirective;
+
+    public slides = [
+      'First slide',
+      'Second slide',
+      'Third slide',
+      'Fourth slide',
+      'Fifth slide',
+      'Sixth slide'
+    ];
 }
--- IIMS/src/app/main/contents/Home/home.module.ts
+++ IIMS/src/app/main/contents/Home/home.module.ts
@@ -5,6 +5,29 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { MaterialModule } from 'src/app/common/Material.module';
+// import { SwiperModule } from 'angular2-useful-swiper';
+import { SwiperModule, SWIPER_CONFIG, SwiperConfigInterface } from 'ngx-swiper-wrapper';
+
+const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+  observer: true,
+    direction: 'horizontal',
+    // threshold: 50,
+    spaceBetween: 5,
+    slidesPerView: 'auto',
+    // centeredSlides: true,
+    navigation: true,
+    autoplay: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      hideOnClick: false
+    },
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    //   hide: false,
+    //   draggable: true
+    // }
+};
 
 const Routes: Routes = [
     {   path: '',
@@ -19,9 +42,16 @@ const Routes: Routes = [
     imports: [
       RouterModule.forRoot(Routes),
       MaterialModule,
+      SwiperModule,
     ],
     exports: [
     ],
+    providers: [
+      {
+        provide:　SWIPER_CONFIG,
+        useValue: DEFAULT_SWIPER_CONFIG
+      }
+    ]
 })
 export class HomeModule {
 }
