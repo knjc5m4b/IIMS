7925e9b38d8486a509a70cd23fb5aa8ee1adfd9b
yuchieh Kuan <yuchieh0725@gmail.com>
2019-06-02 01:15:33 +0800

    0601 meeting

--- IIMS/src/app/main/contents/test/yoriko/yorikotest.component.ts
+++ IIMS/src/app/main/contents/test/yoriko/yorikotest.component.ts
@@ -5,6 +5,25 @@ import { MatSidenav, MatDialog, MatDialogRef } from '@angular/material';
 import { TabDialogComponent } from '../../Home/tab-dialog/tab-dialog.component';
 import Swiper from 'swiper';
 import {MatSelectModule} from '@angular/material/select';
+export interface PeriodicElement {
+    name: string;
+    position: number;
+    weight: number;
+    symbol: string;
+  }
+
+const ELEMENT_DATA: PeriodicElement[] = [
+    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  ];
 
 @Component ({
     selector: 'app-yorikotest-component',
@@ -12,6 +31,8 @@ import {MatSelectModule} from '@angular/material/select';
     styleUrls: ['yorikotest.component.css'],
 })
 export class YorikoTestComponent {
+    displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+    dataSource = ELEMENT_DATA;
     @Input() name: string;
     @Input() content: string;
     @Input() tabname: string;
