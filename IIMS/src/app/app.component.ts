2f9534b9a9374bb96398cf7eeb4eb011dbd87ed0
yuchieh Kuan <yuchieh0725@gmail.com>
2019-06-02 23:03:14 +0800

    menu used roles and tried css

--- IIMS/src/app/app.component.ts
+++ IIMS/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Injector } from '@angular/core';
+import { Food } from './main/contents/test/yoriko/yorikotest.component';
+import { Url } from 'url';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'IIMS';
+  opened: true;
+  menu: Menu[] = [
+    {value: 'role1' , viewValue: '職缺刊登', routerLink: '/postjob'},
+    {value: 'role2' , viewValue: '檢視面試者名單', routerLink: '/postjob'},
+    {value: 'role3' , viewValue: 'Angela', routerLink: '/angela'},
+    {value: 'role4' , viewValue: 'Yoriko', routerLink: '/yoriko'},
+    {value: 'role5' , viewValue: 'Sindy', routerLink: '/sindy'},
+    {value: 'role6' , viewValue: 'Zack', routerLink: '/zack'},
+  ];
+  constructor(
+     injector:　Injector
+  ) {
+    // super(injector);
+  }
+
+}
+export interface Menu {
+  value: string;
+  viewValue: string;
+  routerLink: string;
 }
