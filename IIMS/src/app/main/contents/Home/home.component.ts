f36b57afe7551b7e0a5623a3e39703dd22497003
yuchieh Kuan <yuchieh0725@gmail.com>
2019-09-17 20:06:31 +0800

    slideIMG

--- IIMS/src/app/main/contents/Home/home.component.ts
+++ IIMS/src/app/main/contents/Home/home.component.ts
@@ -3,6 +3,7 @@ import { SwiperComponent, SwiperDirective} from 'ngx-swiper-wrapper';
 import { MatDialog} from '@angular/material';
 import { TabDialogComponent } from './tab-dialog/tab-dialog.component';
 import { FormControl } from '@angular/forms';
+import { SlideDialogComponent } from './slide-dialog/slide-dialog.component';
 
 @Component({
     selector: 'app-home-component',
@@ -27,16 +28,16 @@ export class HomeComponent implements OnInit {
     tabs = [];
     selected = new FormControl(0);
 
-    files: File[] = [];
-    progress: number;
-    dragFiles: any;
-    validComboDrag: any;
-    lastInvalids: any;
-    fileDropDisabled: any;
-    baseDropValid: any;
-    lastFileAt: Date;
+    // files: File[] = [];
+    // progress: number;
+    // dragFiles: any;
+    // validComboDrag: any;
+    // lastInvalids: any;
+    // fileDropDisabled: any;
+    // baseDropValid: any;
+    // lastFileAt: Date;
     slideimage: any;
-    allimages: [];
+    allimages: any[] = [];
     index: number;
 
     public slides = ['First'];
@@ -47,6 +48,15 @@ export class HomeComponent implements OnInit {
     removeTab(index: number) {
         this.tabs.splice(index, 1);
     }
+
+    // addslides() {
+    //     this.slides.push();
+    //     this.selected.setValue(this.slides.length);
+    // }
+
+    // removeslides(index: number) {
+    //     this.slides.splice(index, 1);
+    // }
 
     opendialog(): void {
         const dialogRef = this.dialog.open(TabDialogComponent, {
@@ -66,28 +76,48 @@ export class HomeComponent implements OnInit {
         });
     }
 
-    getUrl(index: number) {
-        let i = 0;
-        for (i = 0; i < this.files.length; i + 1 ) {
-            console.log('i:', i, this.files[i]);
-            // console.log(this.images[i], this.slideimage);
-            // if (this.files.length > 1) {
-            //     this.files.splice(index, 1);
-            // }
-            if (this.files.length < 6) {
-                const reader = new FileReader();
-                reader.readAsDataURL(this.files[i]);
-                reader.onload = () => {
-                    this.slideimage = reader.result;
-                    return this.slideimage;
-                };
+    openSlidedialog(): void {
+        const dialogRef = this.dialog.open(SlideDialogComponent, {
+            width: '600px',
+            // height: '700px',
+            data: {
+                slideimage: this.slideimage,
+                allimages: this.allimages,
             }
-            i = i + 1;
-        }
+        });
+        dialogRef.afterClosed().subscribe(res => {
+            if (res) {
+                this.slideimage = res[0];
+                // this.tabimage = res[1];
+            }
+            // console.log(res);
+        });
     }
 
-    getDate(index: number) {
-        this.getUrl(index);
-        return new Date();
-    }
+    // getUrl(index: number) {
+    //     let i = 0;
+    //     for (i = 0; i < this.files.length; i + 1 ) {
+    //         console.log('i:', i, this.files[i]);
+    //         // console.log(this.images[i], this.slideimage);
+    //         // if (this.files.length > 1) {
+    //         //     this.files.splice(index, 1);
+    //         // }
+    //         if (this.files.length < 6) {
+    //             const reader = new FileReader();
+    //             reader.readAsDataURL(this.files[i]);
+    //             reader.onload = () => {
+    //                 this.slideimage = reader.result;
+    //                 this.allimages.push(this.slideimage);
+    //                 console.log(this.slideimage);
+    //                 return this.slideimage;
+    //             };
+    //         }
+    //         i = i + 1;
+    //     }
+    // }
+
+    // getDate(index: number) {
+    //     this.getUrl(index);
+    //     return new Date();
+    // }
 }
